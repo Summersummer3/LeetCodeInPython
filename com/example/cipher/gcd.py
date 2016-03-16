4aedbbe229d27f788a890f4db49af5d279c10391
summer <techsum8@live.com>
2016-03-16 14:27:55 +0900

    .

--- com/example/cipher/gcd.py
+++ com/example/cipher/gcd.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'summer'
 
+def gcd_1(x, y):
+    if x < 0:
+        x = -x
+    if y < 0:
+        y = -y
+    if x < y:
+        return gcd_1(y, x)
+    else:
+        if y % x == 0:
+            return x
+        else:
+            return gcd_1(y, x-y)
+
 def gcd(x, y):
     if x < 0:
         x = -x
@@ -8,8 +21,11 @@ def gcd(x, y):
         y = -y
     if x < y:
         return gcd(y, x)
+    res = x - (x / y) * y
+    if not res:
+        return y
     else:
-        if y % x == 0:
-            return x
-        else:
-            return gcd(y, x-y)
+        return gcd(y, res)
+
+print gcd(135, 45)
+print gcd_1(135, 45)
