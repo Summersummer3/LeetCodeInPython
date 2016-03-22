a8882b77bb87710235c6bfe1455abc5820310d66
summer <techsum8@live.com>
2016-03-22 16:58:21 +0900

    sqr. and mul,  algo.

---
+++ com/example/cipher/decimalChange.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'summer'
+def decimalToOther(n, m):
+    """
+    n decimal integer
+    m radix number
+    :param n: int
+    :param m: int
+    :return: int
+    """
+    res = ""
+    while n > m:
+        res = str(n % m) + res
+        n /= m
+    res = str(n) + res
+    return int(res)
+
+def decimalToBinary(n):
+    """
+    as the function name
+    :param n:int
+    :return:binary int
+    """
+    return decimalToOther(n, 2)
+
+print decimalToBinary(3533)
