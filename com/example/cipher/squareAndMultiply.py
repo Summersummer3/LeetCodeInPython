7c15e1e248f801d733f38a789ad05e31a14e703c
summer <techsum8@live.com>
2016-03-22 22:46:26 +0900

    add square and multiple algo.

---
+++ com/example/cipher/squareAndMultiply.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'summer'
+from decimalChange import decimalToBinary
+
+def squareAndMultiply(mes, n, b):
+    """
+    :param mes: int, the message
+    :param n: int, = p * q
+    :param b: int, key, we need to count mes ** b
+    :return: e int, res = mes ** b (mod n)
+    """
+    e = mes
+    pk = str(decimalToBinary(b))
+    for bit in pk[1:]:
+        i = int(bit)
+        e = (e ** 2) % n
+        if i:
+            e = (e * mes) % n
+    return e
+
+print squareAndMultiply(9726, 11413, 3533)
