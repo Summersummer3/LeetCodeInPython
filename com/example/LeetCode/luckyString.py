c94b38d112183391c08a1cc33c2218e2ed4ebbf8
summer <techsum8@live.com>
2017-03-23 21:44:31 +0900

    LuckyNum

---
+++ com/example/LeetCode/luckyString.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'summer'
+
+def luckyString(str):
+    """
+    Microsoft coding test
+    :param str:
+    :return:
+    """
+    res = []
+
+    fb = [1, 1]
+    while fb[-1] < 26:
+        fb.append(fb[-1] + fb[-2])
+
+    for i in xrange(len(str)):
+        j = i
+        while j <= len(str):
+            count = 0
+            last = ""
+            for s in str[i:j]:
+                if not s in last:
+                    count += 1
+                    last += s
+            if count in fb and str[i:j] not in res:
+                res.append(str[i:j])
+            j += 1
+    res.sort()
+    for p in res:
+        print p
+
+
+if __name__ == '__main__':
+    str = raw_input()
+    luckyString(str)
