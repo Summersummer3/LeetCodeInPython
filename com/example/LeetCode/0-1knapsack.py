164dc669a681d5ea4c95d0f5c8bfcc271915390d
summer <techsum8@live.com>
2017-04-02 13:12:46 +0900

    0-1snapsackcomplete

--- com/example/LeetCode/0-1knapsack.py
+++ com/example/LeetCode/0-1knapsack.py
@@ -27,7 +27,16 @@ def knapsack_1(lst1, lst2, W):
                 dp[j] = dp[j - lst2[i - 1]] + lst1[i - 1]
     return dp[-1]
 
-print knapsack([2201, 3158, 653, 3043, 1204], [2201, 3158, 653, 3043, 1204], 5129) * 1024
+def completeKnapsack(lst1, lst2, W):
+    dp = [0 if not i else -1 for i in xrange(W + 1)]
+    for i in xrange(1, len(lst1) + 1):
+        for v in xrange(W, -1, -1):
+            if v >= lst2[i - 1] and (dp[v] != -1 or dp[v - lst2[i - 1]] != -1):
+                dp[v] = max(dp[v - lst2[i - 1]] + lst1[i - 1], dp[v])
+        print dp
+    return dp[-1]
+
+print completeKnapsack([10, 20, 30], [5, 5, 5], 10)
 print knapsack_1([2201, 3158, 653, 3043, 1204], [2201, 3158, 653, 3043, 1204], 5129) * 1024
 
 
