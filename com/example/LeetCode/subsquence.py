bcd88127a0274e7ac1e106628ca01dd21496988b
summer <techsum8@live.com>
2017-11-13 13:37:57 +0900

    problem solution

---
+++ com/example/LeetCode/subsquence.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'summer'
+
+def subsequence(num):
+    dp = [0] * len(num)
+    dp[0] = [num[0]]
+    max_l = 1
+    max_i = 0
+
+    for i in xrange(1, len(num)):
+        if num[i] > num[i - 1]:
+            dp[i] = dp[i - 1][:]
+            dp[i].append(num[i])
+
+        else:
+            if i == 1:
+                dp[i] = [num[i]]
+            else:
+                j = i - 2
+                while num[j] > num[i] and j >= 0:
+                    j -= 1
+                if j == -1:
+                    dp[i] = [num[i]]
+                else:
+                    dp[i] = dp[j][:]
+                    dp[i].append(num[i])
+        if len(dp[i]) > max_l:
+            max_l = len(dp[i])
+            max_i = i
+
+    return dp[max_i]
+
+print subsequence([10, 9, 2, 5, 3, 7, 101, 18])
+
